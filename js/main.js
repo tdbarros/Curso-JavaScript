52bd0426be289c644d0cf1c9035eee41abf965e2
Timoteo Barros <tdbarros@gmail.com>
2020-06-04 00:59:35 -0300

    Comit inicial

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var nome = "Nome do infeliz";
+var idade = 40;
+var idade2 = 10;
+
+alert (idade+ idade2)
+
+alert ( "Nome:" + nome + " idade: " + idade + " idade2: " + idade2 + " = " + idade + idade2 );
+
+alert (`Nome:${nome}idade: ${idade}idade2:${idade2} = ${idade}${idade2}` );
