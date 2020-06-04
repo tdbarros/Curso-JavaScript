adb5bc20f9a4308965f4b756448cc1dc499e9bff
Timoteo Barros <tdbarros@gmail.com>
2020-06-04 01:41:51 -0300

    após video 1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-var nome = "Nome do infeliz";
-var idade = 40;
-var idade2 = 10;
+//aula sobre manipulação de variaveis
+//var nome = "Nome do infeliz";
+//var idade = 40;
+//var idade2 = 10;
+//var frase = "Frase de teste com origem"
 
-alert (idade+ idade2)
+//alert (idade+ idade2)
 
-alert ( "Nome:" + nome + " idade: " + idade + " idade2: " + idade2 + " = " + idade + idade2 );
+//alert ( "Nome:" + nome + " idade: " + idade + " idade2: " + idade2 + " = " + idade + idade2 );
 
-alert (`Nome:${nome}idade: ${idade}idade2:${idade2} = ${idade}${idade2}` );
+//alert (`Nome:${nome}idade: ${idade}idade2:${idade2} = ${idade}${idade2}` );
+
+
+//console.log(nome);
+//console.log(idade);
+//console.log(idade2);
+
+//console.log(frase.replace("com","sem"));
+
+//alert (frase.replace("origem","destino")+ ", mas sem origem");
+
+//aula sobre array
