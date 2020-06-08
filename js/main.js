e0ea6c78e29b4f03bef1bed906f56d87d6e02d65
Timoteo Barros <tdbarros@gmail.com>
2020-06-08 01:35:43 -0300

    após video 2

--- js/main.js
+++ js/main.js
@@ -18,5 +18,42 @@
 //console.log(frase.replace("com","sem"));
 
 //alert (frase.replace("origem","destino")+ ", mas sem origem");
-
+//------------------------------------------------
 //aula sobre array
+
+//var lista = ["maça", "pera" , "laranja", "melancia", "limao"];
+//console.log(lista);
+
+//console.log(lista[0]);
+//console.log(lista[1]);
+//console.log(lista[2]);
+//console.log(lista[3]);
+//console.log(lista[4]);
+//lista.pop();
+//console.log(lista);
+//lista.push("uva");
+//console.log(lista);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.sort());
+//console.log(lista.shift());
+//console.log(lista.toString());
+//console.log(lista.join(" O\/O "))
+
+//------------------------------
+// Dicionário
+
+var fruta = { nome:"maçã",
+              cor:"vermelha" }
+
+console.log(fruta.cor,fruta.nome) 
+
+
+var frutas = [{ nome:"maçã", cor:"vermelha" }
+             ,{ nome:"laranja", cor:"laranja" }
+             ,{ nome:"limão", cor:"verde" }
+             ,{ nome:"uva", cor:"roxo" }
+             ,{ nome:"abacaxi", cor:"amarelo"}];
+
+console.log(frutas);
+             
