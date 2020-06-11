64cefd9ff52a54162c3ecbcf3e4f666be1d6d367
Timoteo Barros <tdbarros@gmail.com>
2020-06-11 02:41:14 -0300

    após video 3-4

--- js/main.js
+++ js/main.js
@@ -43,17 +43,116 @@
 //------------------------------
 // Dicionário
 
-var fruta = { nome:"maçã",
-              cor:"vermelha" }
+//var fruta = { nome:"maçã",
+//              cor:"vermelha" }
 
-console.log(fruta.cor,fruta.nome) 
+//console.log(fruta.cor,fruta.nome) 
 
 
-var frutas = [{ nome:"maçã", cor:"vermelha" }
-             ,{ nome:"laranja", cor:"laranja" }
-             ,{ nome:"limão", cor:"verde" }
-             ,{ nome:"uva", cor:"roxo" }
-             ,{ nome:"abacaxi", cor:"amarelo"}];
+//var frutas = [{ nome:"maçã", cor:"vermelha" }
+//             ,{ nome:"laranja", cor:"laranja" }
+//             ,{ nome:"limão", cor:"verde" }
+//             ,{ nome:"uva", cor:"roxo" }
+//             ,{ nome:"abacaxi", cor:"amarelo"}];
 
-console.log(frutas);
-             
+//console.log(frutas);
+
+/*
+// condicional
+var idade = prompt("Qual sua idade?");
+
+if (idade >= 18){
+    console.log(idade);
+    console.log("maior de idade");
+} else {
+    console.log(idade);
+    console.log("menor de idade");
+};
+*/
+
+/*
+//lacos de repeticao
+// while
+var count = 0;
+
+while (count <= 5){
+    console.log(count);
+    count = count +1;
+
+};
+
+//for
+
+var contador;
+
+for(contador=0;contador<=10;contador++) {
+
+    console.log(contador);
+
+};
+
+*/
+
+/*
+var d = new Date();
+console.log(d);
+console.log(d.getDay());
+console.log(d.getMonth());
+console.log(d.getUTCFullYear());
+console.log(d.getUTCHours());
+console.log(d.getUTCMilliseconds());
+console.log(d.getTime());
+console.log(d.getTimezoneOffset());
+console.log(d.getUTCDate());
+console.log(d.);
+*/
+/*
+function soma(n1,n2){
+    return n1+n2
+
+}
+*/
+/*
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+};
+
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    } else {
+        validar = false
+    }
+
+    return validar;
+
+}
+
+alert(soma(5 , 10));
+alert(setReplace("Vai Japao", "Japao", "Brasil"));
+
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+*/
+
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!!!</b>"
+
+//    alert("Clicou!");
+}
+
+function redirecionar(){
+    window.open("https://google.com/"); 
+    window.location.href = "https://gogle.com" ;
+}
+
+function trocar(elemento){
+//    document.getElementById("mouseover").innerHTML = "<b>Obrigado por passar o mouse!!!</b>" 
+    elemento.innerHTML = "Obrigado por passar o mouse!!!"
+} 
+
+function voltar(elemento){
+//    document.getElementById("mouseover").innerHTML = "<b>Passe o mouse aqui!!!</b>" 
+    elemento.innerHTML = "Passe o mouse aqui!!!!"
+}
